575e47f8ae390fce72327ad25e69ca543ac10367
maxi-nm <em.nietomontoya@outlook.com>
2022-03-18 18:36:52 -0300

    dependecies added

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const app = express();
