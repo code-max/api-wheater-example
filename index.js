3ed719cac99fd77c68443b94bea7dab1995d3a78
maxi-nm <em.nietomontoya@outlook.com>
2022-03-18 18:37:29 -0300

    middlewares added

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
